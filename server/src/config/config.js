f70d236de9b56b936f730f3a93b0ce925ae2b484
Abhinav Krishna C K <abbyck@users.noreply.github.com>
2018-01-08 23:16:59 +0530

    Updating user

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -1,9 +1,9 @@
 module.exports = {
   port: process.env.PORT || 8081,
   db: {
-    database: process.env.DB_NAME || 'tabtracker',
-    user: process.env.DB_USER || 'tabtracker',
-    password: process.env.DB_PASS || 'tabtracker',
+    database: process.env.DB_NAME || 'tracker',
+    user: process.env.DB_USER || 'tracker',
+    password: process.env.DB_PASS || 'tracker',
     options: {
       host: process.env.HOST || 'localhost',
       dialect: process.env.dialect || 'sqlite',
